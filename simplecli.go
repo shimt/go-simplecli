03f578879afbffbdd17a3031dae9f6bd43e281ff
Shinichi MOTOKI <shinichi-motoki@overruns.org>
2020-03-23 19:24:25 +0900

    Remove empty function

--- simplecli.go
+++ simplecli.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package simplecli
 package simplecli
 
 import (
@@ -312,7 +313,4 @@ func NewCLI() (cli *CLI) {
 	cli.InitializeError = cli.Initialize()
 
 	return
-}
-
-func init() {
 }
